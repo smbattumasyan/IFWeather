233d612109649f6552cabfe962551ef9235bb768
smbatt <smbatt@mail.com>
2021-07-15 21:59:03 +0400

    first commit

---
+++ IFWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IFWeather
+//
+//  Created by ՍՄԲԱՏ ԹՈՒՄԱՍՅԱՆ on 15.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
